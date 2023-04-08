b1766c31dbdc6457074be75484df0bd4d005b734
vmsrinivas82@gmail.com <vmsrinivas82@gmail.com>
2023-04-08 20:23:19 +0530

    8080 opened

--- instances.tf
+++ instances.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "ntier_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "allows 22 port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
